a084bd799e982c9bca8b7b093b3d203dc9caeab5
REBOOTERS <foreverlove.zyl@163.com>
2017-09-12 08:50:32 +0800

    add  practice6

---
+++ PracticeDraw6/app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice02Rotation.java
@@ -0,0 +1,80 @@
+package com.hencoder.hencoderpracticedraw6.practice;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.hencoder.hencoderpracticedraw6.R;
+
+public class Practice02Rotation extends RelativeLayout {
+    Button animateBt;
+    ImageView imageView;
+    TextView actionTv;
+
+    int animState;
+
+    public Practice02Rotation(Context context) {
+        super(context);
+    }
+
+    public Practice02Rotation(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice02Rotation(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        animateBt = (Button) findViewById(R.id.animateBt);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        actionTv = (TextView) findViewById(R.id.action);
+
+        animateBt.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                // // TODO 在这里处理点击事件，通过 View.animate().rotation/X/Y() 来让 View 旋转
+
+                switch (animState) {
+                    case 0:
+                        imageView.animate().rotation(180);
+                        actionTv.setText("rotation(100)");
+                        break;
+                    case 1:
+                        imageView.animate().rotation(0);
+                        actionTv.setText("rotation(0)");
+                        break;
+                    case 2:
+                        imageView.animate().rotationX(180);
+                        actionTv.setText("rotationX(180)");
+                        break;
+                    case 3:
+                        imageView.animate().rotationX(0);
+                        actionTv.setText("rotationX(0)");
+                        break;
+                    case 4:
+                        imageView.animate().rotationY(180);
+                        actionTv.setText("rotationY(180)");
+                        break;
+                    case 5:
+                        imageView.animate().rotationY(0);
+                        actionTv.setText("rotationY(0)");
+                        break;
+
+
+                }
+                animState++;
+                animState = animState == 6 ? 0 : animState;
+
+            }
+        });
+    }
+}
