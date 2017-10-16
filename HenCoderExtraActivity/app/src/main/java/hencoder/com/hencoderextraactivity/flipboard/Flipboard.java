b3127f22a1f43908af2c9f7eacba89735da69120
REBOOTERS <foreverlove.zyl@163.com>
2017-10-16 19:46:15 +0800

    missing files add

---
+++ HenCoderExtraActivity/app/src/main/java/hencoder/com/hencoderextraactivity/flipboard/Flipboard.java
@@ -0,0 +1,32 @@
+package hencoder.com.hencoderextraactivity.flipboard;
+
+/**
+ * Created by engineer on 2017/10/14.
+ */
+
+public class Flipboard {
+    public int degreeY;
+    public int degreeZ;
+
+
+    public Flipboard(int degreeY, int degreeZ) {
+        this.degreeY = degreeY;
+        this.degreeZ = degreeZ;
+    }
+
+    public int getDegreeY() {
+        return degreeY;
+    }
+
+    public void setDegreeY(int degreeY) {
+        this.degreeY = degreeY;
+    }
+
+    public int getDegreeZ() {
+        return degreeZ;
+    }
+
+    public void setDegreeZ(int degreeZ) {
+        this.degreeZ = degreeZ;
+    }
+}
---
+++ HenCoderExtraActivity/app/src/main/java/hencoder/com/hencoderextraactivity/ruler/RulerView.java
@@ -0,0 +1,27 @@
+package hencoder.com.hencoderextraactivity.ruler;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by engineer on 2017/10/15.
+ */
+
+public class RulerView extends View {
+
+    public RulerView(Context context) {
+        super(context);
+    }
+
+    public RulerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RulerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
